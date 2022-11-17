9c508270c4751dde7673b4b40cb02d24f739f70e
Narek <mirzakhanyan99@list.ru>
2022-11-17 18:26:31 +0300

    Готовое третье задание

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("Введите число ");
+
+int myNum = int.Parse(Console.ReadLine());
+
+if (myNum % 2 == 0)
+{
+    System.Console.WriteLine("Ваше число " + myNum + " чётное");
+}
+else
+{
+    System.Console.WriteLine("Ваше число " + myNum + " нечётное");
+}
+Console.ReadLine();
