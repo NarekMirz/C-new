9c5db4fbf5e17eee82463a0e7eff37e494aedc41
Narek <mirzakhanyan99@list.ru>
2022-11-17 18:21:05 +0300

    Готовое второе задание

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Введите три числа ");
+
+int number1,number2,number3;
+
+number1 = int.Parse(Console.ReadLine());
+number2 = int.Parse(Console.ReadLine());
+number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+if(max > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+System.Console.WriteLine("Максимальное = " + max);
